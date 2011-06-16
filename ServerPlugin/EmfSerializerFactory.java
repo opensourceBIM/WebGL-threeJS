7faa38bbbfdb2c1c751ece4bc92402168ce45e5f
hlg <public.void@gmx.de>
2011-06-16 09:46:15 +0200

    backend

---
+++ ServerPlugin/EmfSerializerFactory.java
@@ -0,0 +1,220 @@
+package org.bimserver.serializers;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.ServiceLoader;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+import nl.tue.buildingsmart.express.dictionary.SchemaDefinition;
+
+import org.bimserver.SettingsManager;
+import org.bimserver.citygml.CityGmlSerializer;
+import org.bimserver.collada.ColladaSerializer;
+import org.bimserver.collada.KmzSerializer;
+import org.bimserver.exceptions.NoSerializerFoundException;
+import org.bimserver.ifc.EmfSerializer;
+import org.bimserver.ifc.FieldIgnoreMap;
+import org.bimserver.ifc.IfcModel;
+import org.bimserver.ifc.PackageDefinition;
+import org.bimserver.ifc.SerializerException;
+import org.bimserver.ifc.file.writer.IfcStepSerializer;
+import org.bimserver.ifc.xml.writer.IfcXmlSerializer;
+import org.bimserver.ifcengine.IfcEngineFactory;
+import org.bimserver.models.store.Project;
+import org.bimserver.models.store.User;
+import org.bimserver.o3d.O3dJsonSerializer;
+import org.bimserver.shared.ResourceFetcher;
+import org.bimserver.shared.ResultType;
+import org.bimserver.shared.ResultType.DefaultSelected;
+import org.bimserver.shared.ResultType.Type;
+import org.bimserver.shared.ResultType.UseInCheckout;
+import org.bimserver.shared.ResultType.UserType;
+import org.bimserver.version.Version;
+
+public class EmfSerializerFactory {
+	
+	public abstract class EmfSerializerCreator {
+		public abstract EmfSerializer create(Project project, User user, IfcModel model, String fileName) throws SerializerException;
+	}
+
+	Comparator<ResultType> resultTypeComparator = new Comparator<ResultType>(){
+		@Override
+		public int compare(ResultType o1, ResultType o2) {
+			return o1.compareTo(o2);
+		}};
+	
+	private static final EmfSerializerFactory INSTANCE = new EmfSerializerFactory();
+	private final Map<String, ResultType> resultTypes = new TreeMap<String, ResultType>();
+	private final Map<ResultType, EmfSerializerCreator> serializerCreators = new HashMap<ResultType, EmfSerializerCreator>();
+	private FieldIgnoreMap fieldIgnoreMap;
+	private PackageDefinition colladaSettings;
+	private SchemaDefinition schemaDefinition;
+	private Version version;
+
+	private IfcEngineFactory ifcEngineFactory;
+	private ResourceFetcher resourceFetcher;
+	private SettingsManager settingsManager;
+
+	private EmfSerializerFactory() {
+	}
+
+	public static EmfSerializerFactory getInstance() {
+		return INSTANCE;
+	}
+
+	public void init(Version version, SchemaDefinition schemaDefinition, FieldIgnoreMap fieldIgnoreMap, IfcEngineFactory ifcEngineFactory, PackageDefinition colladaSettings, ResourceFetcher resourceFetcher, SettingsManager settingsManager) {
+		this.version = version;
+		this.schemaDefinition = schemaDefinition;
+		this.fieldIgnoreMap = fieldIgnoreMap;
+		this.ifcEngineFactory = ifcEngineFactory;
+		this.colladaSettings = colladaSettings;
+		this.resourceFetcher = resourceFetcher;
+		this.settingsManager = settingsManager;
+	}
+	
+	public void register(ResultType resultType, EmfSerializerCreator emfSerializerCreator) {
+		resultTypes.put(resultType.getName(), resultType);
+		serializerCreators.put(resultType, emfSerializerCreator);
+	}
+
+	public void unregister(ResultType resultType, EmfSerializerCreator emfSerializerCreator) {
+		resultTypes.remove(resultType.getName());
+		serializerCreators.remove(resultType);
+	}
+	
+	public EmfSerializer create(Project project, User user, ResultType resultType, IfcModel model, String name) throws NoSerializerFoundException, SerializerException {
+		EmfSerializerCreator emfSerializerCreator = serializerCreators.get(resultType);
+		if (emfSerializerCreator == null) {
+			throw new NoSerializerFoundException("Serializer for type " + resultType.getNiceName() + " not found or not enabled");
+		}
+		return emfSerializerCreator.create(project, user, model, name);
+	}
+
+	public Set<ResultType> getMultipleResultTypes() {
+		Set<ResultType> result = new TreeSet<ResultType>(resultTypeComparator);
+		for (ResultType resultType : resultTypes.values()) {
+			if (resultType.getMultiple() == Type.MULTIPLE) {
+				result.add(resultType);
+			}
+		}
+		return result;
+	}
+
+	public Set<ResultType> getSingleResultTypes() {
+		Set<ResultType> result = new TreeSet<ResultType>(resultTypeComparator);
+		for (ResultType resultType : resultTypes.values()) {
+			if (resultType.getMultiple() == Type.SINGLE) {
+				result.add(resultType);
+			}
+		}
+		return result;
+	}
+
+	public void initSerializers() {
+		String enabledExportTypesString = settingsManager.getSettings().getEnabledExportTypes();
+		String[] enabledExportTypes = enabledExportTypesString.split(",");
+		Set<String> enabled = new HashSet<String>();
+		for (String type : enabledExportTypes) {
+			String upperCase = type.trim().toUpperCase();
+			if (!upperCase.equals("")) {
+				enabled.add(upperCase);
+			}
+		}
+		register(new ResultType("JSON_MODEL_FORMAT_2", "JSON Model Formal 2.0", "js", "appliction/collada", UseInCheckout.USE_NOT_IN_CHECKOUT, UserType.USER_TYPE, Type.MULTIPLE, DefaultSelected.FALSE, enabled.contains("JSON_MODEL_FORMAT_2")), new EmfSerializerCreator() {
+			@Override
+			public EmfSerializer create(Project project, User user, IfcModel model, String fileName) throws SerializerException {
+				JSONModelFormat2Serializer jsonModelFormat2Serializer = new JSONModelFormat2Serializer();				
+				jsonModelFormat2Serializer.init(project, user, fileName, model, fieldIgnoreMap, schemaDefinition, ifcEngineFactory);
+				return jsonModelFormat2Serializer;
+			}
+		});
+		register(new ResultType("IFC", "IFC2x3", "ifc", "application/ifc", UseInCheckout.USE_IN_CHECKOUT, UserType.USER_TYPE, Type.MULTIPLE, DefaultSelected.TRUE, enabled.contains("IFC")), new EmfSerializerCreator(){
+			@Override
+			public EmfSerializer create(Project project, User user, IfcModel model, String fileName) {
+				IfcStepSerializer ifcSerializer = new IfcStepSerializer(project, user, fileName, model, schemaDefinition);
+				ifcSerializer.setFileDescription("Bimserver.org " + version.getVersion() + " generated IFC file");
+				ifcSerializer.setPreProcessorVersion("Bimserver.org " + version.getVersion());
+				return ifcSerializer;
+			}
+		});
+		register(new ResultType("IFCXML", "IFCXML2x3", "ifcxml", "application/ifcxml", UseInCheckout.USE_IN_CHECKOUT, UserType.USER_TYPE, Type.MULTIPLE, DefaultSelected.FALSE, enabled.contains("IFCXML")), new EmfSerializerCreator() {
+			@Override
+			public EmfSerializer create(Project project, User user, IfcModel model, String fileName) throws SerializerException {
+				return new IfcXmlSerializer(fileName, model, schemaDefinition);
+			}
+		});
+		register(new ResultType("IFCTXT", "Plain text IFC", "txt", "text", UseInCheckout.USE_NOT_IN_CHECKOUT, UserType.USER_TYPE, Type.MULTIPLE, DefaultSelected.FALSE, enabled.contains("IFCTXT")), new EmfSerializerCreator() {
+			@Override
+			public EmfSerializer create(Project project, User user, IfcModel model, String fileName) throws SerializerException {
+				IfcStepSerializer ifcSerializer = new IfcStepSerializer(project, user, fileName, model, schemaDefinition);
+				ifcSerializer.setFileDescription("Bimserver.org " + version.getVersion() + " generated IFC file");
+				ifcSerializer.setPreProcessorVersion("Bimserver.org " + version.getVersion());
+				return ifcSerializer;
+			}
+		});
+		register(new ResultType("CITYGML", "City GML 1.0.0", "gml", "application/gml", UseInCheckout.USE_NOT_IN_CHECKOUT, UserType.USER_TYPE, Type.MULTIPLE, DefaultSelected.FALSE, enabled.contains("CITYGML")), new EmfSerializerCreator() {
+			@Override
+			public EmfSerializer create(Project project, User user, IfcModel model, String fileName) throws SerializerException {
+				CityGmlSerializer cityGmlSerializer = new CityGmlSerializer();
+				cityGmlSerializer.init(project, user, fileName, model, schemaDefinition, fieldIgnoreMap, ifcEngineFactory);
+				return cityGmlSerializer;
+			}
+		});
+		register(new ResultType("COLLADA", "Collada", "dae", "appliction/collada", UseInCheckout.USE_NOT_IN_CHECKOUT, UserType.USER_TYPE, Type.MULTIPLE, DefaultSelected.FALSE, enabled.contains("COLLADA")), new EmfSerializerCreator() {
+			@Override
+			public EmfSerializer create(Project project, User user, IfcModel model, String fileName) throws SerializerException {
+				ColladaSerializer colladaSerializer = new ColladaSerializer();
+				colladaSerializer.init(project, user, fileName, model, schemaDefinition, fieldIgnoreMap, ifcEngineFactory, colladaSettings);
+				return colladaSerializer;
+			}
+		});
+		register(new ResultType("KMZ", "KMZ", "kmz", "application/vnd.google-earth.kmz", UseInCheckout.USE_NOT_IN_CHECKOUT, UserType.USER_TYPE, Type.MULTIPLE, DefaultSelected.FALSE, enabled.contains("KMZ")), new EmfSerializerCreator() {
+			@Override
+			public EmfSerializer create(Project project, User user, IfcModel model, String fileName) throws SerializerException {
+				KmzSerializer kmzSerializer = new KmzSerializer();
+				kmzSerializer.init(project, user, fileName, model, schemaDefinition, fieldIgnoreMap, ifcEngineFactory, colladaSettings);
+				return kmzSerializer;
+			}
+		});
+		register(new ResultType("O3D_JSON", "WebGL", "o3djson", "appliction/json", UseInCheckout.USE_NOT_IN_CHECKOUT, UserType.USER_TYPE, Type.MULTIPLE, DefaultSelected.FALSE, enabled.contains("O3D_JSON")), new EmfSerializerCreator() {
+			@Override
+			public EmfSerializer create(Project project, User user, IfcModel model, String fileName) throws SerializerException {
+				O3dJsonSerializer o3dJsonSerializer = new O3dJsonSerializer();
+				o3dJsonSerializer.init(project, user, fileName, model, fieldIgnoreMap, schemaDefinition, ifcEngineFactory);
+				return o3dJsonSerializer;
+			}
+		});
+		register(new ResultType("OBJECT_INFO", "Object Info", "html", "text/html", UseInCheckout.USE_NOT_IN_CHECKOUT, UserType.USER_TYPE, Type.SINGLE, DefaultSelected.FALSE, enabled.contains("OBJECT_INFO")), new EmfSerializerCreator() {
+			@Override
+			public EmfSerializer create(Project project, User user, IfcModel model, String fileName) {
+				ObjectInfoSerializer objectInfoSerializer = new ObjectInfoSerializer();
+				objectInfoSerializer.init(fileName, model);
+				return objectInfoSerializer;
+			}
+		});
+	}
+
+	public ResultType getResultType(String resultTypeName) {
+		return resultTypes.get(resultTypeName);
+	}
+
+	public boolean resultTypeEnabled(String resultTypeName) {
+		return getResultType(resultTypeName).isEnabled();
+	}
+
+	public Set<String> getAllSerializerClassNames() {
+		Set<String> classNames = new HashSet<String>();
+		ServiceLoader<EmfSerializer> loader = ServiceLoader.load(EmfSerializer.class);
+		Iterator<EmfSerializer> iterator = loader.iterator();
+		while (iterator.hasNext()) {
+			EmfSerializer next = iterator.next();
+			classNames.add(next.getClass().getSimpleName());
+		}
+		return classNames;
+	}
+}
---
+++ ServerPlugin/JSONModelFormat2Serializer.java
@@ -0,0 +1,282 @@
+package org.bimserver.serializers;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import nl.tue.buildingsmart.express.dictionary.SchemaDefinition;
+
+import org.bimserver.emf.IdEObject;
+import org.bimserver.ifc.BimModelSerializer;
+import org.bimserver.ifc.EmfSerializer;
+import org.bimserver.ifc.FieldIgnoreMap;
+import org.bimserver.ifc.IfcModel;
+import org.bimserver.ifc.SerializerException;
+import org.bimserver.ifc.file.writer.IfcStepSerializer;
+import org.bimserver.ifcengine.FailSafeIfcEngine;
+import org.bimserver.ifcengine.Geometry;
+import org.bimserver.ifcengine.IfcEngineException;
+import org.bimserver.ifcengine.IfcEngineFactory;
+import org.bimserver.ifcengine.IfcEngineModel;
+import org.bimserver.ifcengine.Instance;
+import org.bimserver.ifcengine.SurfaceProperties;
+import org.bimserver.ifcengine.jvm.IfcEngine.InstanceVisualisationProperties;
+import org.bimserver.models.ifc2x3.IfcColumn;
+import org.bimserver.models.ifc2x3.IfcDoor;
+import org.bimserver.models.ifc2x3.IfcRailing;
+import org.bimserver.models.ifc2x3.IfcRamp;
+import org.bimserver.models.ifc2x3.IfcRoof;
+import org.bimserver.models.ifc2x3.IfcRoot;
+import org.bimserver.models.ifc2x3.IfcSlab;
+import org.bimserver.models.ifc2x3.IfcStair;
+import org.bimserver.models.ifc2x3.IfcStairFlight;
+import org.bimserver.models.ifc2x3.IfcWall;
+import org.bimserver.models.ifc2x3.IfcWallStandardCase;
+import org.bimserver.models.ifc2x3.IfcWindow;
+import org.bimserver.models.store.Project;
+import org.bimserver.models.store.User;
+import org.bimserver.o3d.BinaryIndexBuffer;
+import org.bimserver.o3d.BinaryVertexBuffer;
+import org.bimserver.o3d.SetGeometryResult;
+import org.eclipse.emf.ecore.EObject;
+import org.mangosdk.spi.ProviderFor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@ProviderFor(value=EmfSerializer.class)
+public class JSONModelFormat2Serializer extends BimModelSerializer {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(JSONModelFormat2Serializer.class);
+	private Project project;
+	private User user;
+	private SchemaDefinition schemaDefinition;
+	private FailSafeIfcEngine ifcEngine;	
+	
+	public void init(Project project, User user, String fileName, IfcModel model, FieldIgnoreMap fieldIgnoreMap, SchemaDefinition schemaDefinition, IfcEngineFactory ifcEngineFactory) throws SerializerException {
+		super.init(fileName, model, fieldIgnoreMap);
+		this.project = project;
+		this.user = user;
+		this.schemaDefinition = schemaDefinition;
+		try {
+			this.ifcEngine = ifcEngineFactory.createFailSafeIfcEngine();
+		} catch (IfcEngineException e) {
+			throw new SerializerException(e);
+		}
+	}
+	
+	@Override
+	protected void reset() {
+		setMode(Mode.BODY);
+	}
+	
+	@Override
+	public boolean write(OutputStream outputStream) throws SerializerException {
+		//System.out.println("...");
+		if (getMode() == Mode.BODY) {
+			List<SetGeometryResult> geometries = getGeometries();
+			
+			PrintWriter out = new PrintWriter(outputStream);
+			out.println("var model = [");
+			
+			for(SetGeometryResult geometry : geometries) {
+				out.println("{");
+				writeGeometry(out, geometry);
+				out.println("},");
+			}
+			
+			out.println("");
+			out.println("];");
+			out.println("");		
+			out.println("  postMessage( model );");
+			out.flush();
+			
+			setMode(Mode.FINISHED);
+			return true;
+		} else if (getMode() == Mode.FINISHED) {
+			return false;
+		}
+		return false;
+	}
+	
+	private void writeGeometry(PrintWriter out, SetGeometryResult geometry) {
+		out.println("id : '" + geometry.getGuid() +  "', ");
+		out.println("geometry : {");
+		out.println(" 'version' : 2, ");
+		out.println("  'materials': [ {" +
+		        		"\"DbgColor\" : 0xeeeeee," +
+		        		"\"DbgIndex\" : 0," + 
+		        		"\"DbgName\" : \"dummy\"," +
+		        		"\"colorDiffuse\" : [ 1, 0, 0 ]" + 
+		    		"} ],");
+		out.print("  'vertices': [ ");
+
+		List<Float> vertices = geometry.getBinaryVertexBuffer().getVertices();
+		if (vertices != null && vertices.size() > 0) {		
+			for (int i = 0; i < vertices.size(); i++) {	
+				out.print(i==0 ? "" : ",");
+				out.print(i % 3 == 0 ? " " : "");
+				out.print(vertices.get(i));
+			}
+		}
+		
+		out.println(" ], ");
+		out.print("  'normals':  [");
+		
+		List<Float> normals = geometry.getBinaryVertexBuffer().getNormals();
+		if (normals != null && normals.size() > 0) {
+			for (int i = 0; i < normals.size(); i++) {
+				out.print(i==0 ? "" : ",");
+				out.print(i % 3 == 0 ? " " : "");
+				out.print(normals.get(i));
+			}
+		}
+		
+		out.println("  ],");
+		out.println("  'colors':   [ ],");
+		out.println("  'uvs':      [ ],");
+		out.print("  'faces': [ ");
+		
+		List<Integer> indices = geometry.getBinaryIndexBuffer().getIndices();
+		if (indices != null && indices.size() > 0) {		
+			for (int i = 0; i < indices.size(); i+=3) {
+				out.print(i==0 ? "" : ",");
+				out.print(" 32, ");
+				out.print((indices.get(i)) + "," + (indices.get(i+1)) + "," + (indices.get(i+2)) + ",");
+				out.print((indices.get(i)) + "," + (indices.get(i+1)) + "," + (indices.get(i+2)));
+			}
+		}
+		
+		
+		out.println("  ]");
+		out.println("  }");
+		out.println("");
+	}
+	
+	
+//	private void writeFaces(PrintWriter out, List<SetGeometryResult> geometries) {
+//		int offset = 0;
+//		for (int i = 0; i < geometries.size(); i++) {
+//			SetGeometryResult geometry = geometries.get(i);
+//			if (geometry == null) { continue; }
+//			List<Integer> indices = geometry.getBinaryIndexBuffer().getIndices();
+//			if (indices == null || indices.size() == 0) { continue; }
+//			
+//			for (int j = 0; j < indices.size(); j+=3) {
+//				out.print((i==0 && j==0) ? "" : ",");
+//				out.print(" 32, ");
+//				out.print((offset + indices.get(j)) + "," + (offset + indices.get(j+1)) + "," + (offset + indices.get(j+2)) + ",");
+//				out.print((offset + indices.get(j)) + "," + (offset + indices.get(j+1)) + "," + (offset + indices.get(j+2)));
+//			}
+//			offset += (geometry.getBinaryVertexBuffer().getVertices().size() / 3);
+//			System.out.println("Indices: " + indices.size());
+//		}
+//	}
+
+//	private void writeVertices(PrintWriter out, List<SetGeometryResult> geometries) {
+//		for (int i = 0; i < geometries.size(); i++) {
+//			SetGeometryResult geometry = geometries.get(i);
+//			if (geometry == null) { continue; }
+//			List<Float> vertices = geometry.getBinaryVertexBuffer().getVertices();
+//			if (vertices == null || vertices.size() == 0) { continue; }
+//			
+//			for (int j = 0; j < vertices.size(); j++) {
+//				out.print((i==0 && j==0) ? "" : ",");
+//				out.print(j % 3 == 0 ? " " : "");
+//				out.print(vertices.get(j));
+//			}
+//			System.out.println("Vertices: " + vertices.size());
+//
+//		}
+//	}
+	
+//	private void writeNormals(PrintWriter out, List<SetGeometryResult> geometries) {
+//		for (int i = 0; i < geometries.size(); i++) {
+//			SetGeometryResult geometry = geometries.get(i);
+//			if (geometry == null) { continue; }
+//			List<Float> normals = geometry.getBinaryVertexBuffer().getNormals();
+//			if (normals == null || normals.size() == 0) { continue; }
+//			
+//			for (int j = 0; j < normals.size(); j++) {
+//				out.print((i==0 && j==0) ? "" : ",");
+//				out.print(j % 3 == 0 ? " " : "");
+//				out.print(normals.get(j));
+//			}
+//			System.out.println("Normals: " + normals.size());
+//		}
+//
+//	}
+	
+	private List<SetGeometryResult> getGeometries() {
+		List<SetGeometryResult> geometryList = new ArrayList<SetGeometryResult>();
+		//Class[] eClasses = new Class[] { IfcSlab.class, IfcRoof.class, IfcWall.class, IfcWallStandardCase.class, IfcWindow.class, IfcDoor.class, IfcColumn.class, IfcRamp.class,
+		//				IfcStair.class, IfcStairFlight.class, IfcRailing.class };
+		 Class[] eClasses = new Class[] { IfcWallStandardCase.class, IfcWall.class, IfcWindow.class, IfcDoor.class };
+
+		try {
+			for (Class<? extends EObject> eClass : eClasses) {
+				int i=0;
+				for (Object object : model.getAll(eClass)) {
+					i++;
+		//			if (i < 2) { continue; }
+					IfcRoot ifcRoot = (IfcRoot) object;
+					SetGeometryResult geometry = getGeometry(ifcRoot);
+					if (geometry != null) { geometryList.add(geometry); }
+			//		if (i == 13) { break; }
+					
+				}
+			}
+		}  catch (SerializerException e) {
+			LOGGER.error("", e);
+		} catch (IfcEngineException e) {
+			LOGGER.error("", e);
+		}
+		return geometryList;
+	}
+
+	
+	private SetGeometryResult getGeometry(IdEObject ifcRootObject) throws SerializerException, IfcEngineException {
+		IfcModel IfcModel = new IfcModel();
+		convertToSubset(ifcRootObject.eClass(), ifcRootObject, IfcModel, new HashMap<EObject, EObject>());
+		IfcStepSerializer ifcSerializer = new IfcStepSerializer(project, user, "", IfcModel, schemaDefinition);
+		BinaryIndexBuffer binaryIndexBuffer = new BinaryIndexBuffer();
+		BinaryVertexBuffer binaryVertexBuffer = new BinaryVertexBuffer();
+		IfcEngineModel model = ifcEngine.openModel(ifcSerializer.getBytes());
+		try {
+			SurfaceProperties sp = model.initializeModelling();
+			model.setPostProcessing(true);
+			Geometry geometry = model.finalizeModelling(sp);
+			int nrIndices = 0;
+			if (geometry != null) {
+				for (Instance instance : model.getInstances(ifcRootObject.eClass().getName().toUpperCase())) {
+					InstanceVisualisationProperties instanceInModelling = instance.getVisualisationProperties();
+					for (int i = instanceInModelling.getStartIndex(); i < instanceInModelling.getPrimitiveCount() * 3 + instanceInModelling.getStartIndex(); i += 3) {
+						binaryIndexBuffer.addIndex(geometry.getIndex(i));
+						binaryIndexBuffer.addIndex(geometry.getIndex(i + 2));
+						binaryIndexBuffer.addIndex(geometry.getIndex(i + 1));
+						nrIndices++;
+					}
+				}
+				for (int i = 0; i < geometry.getNrVertices(); i += 3) {
+					binaryVertexBuffer.addVertex(geometry.getVertex(i));
+					binaryVertexBuffer.addVertex(geometry.getVertex(i + 1));
+					binaryVertexBuffer.addVertex(geometry.getVertex(i + 2));
+				}
+				for (int i = 0; i < geometry.getNrNormals(); i += 3) {
+					binaryVertexBuffer.addNormal(geometry.getNormal(i));
+					binaryVertexBuffer.addNormal(geometry.getNormal(i + 1));
+					binaryVertexBuffer.addNormal(geometry.getNormal(i + 2));
+				}
+				String guid = Long.toString(ifcRootObject.getOid()); // TODO
+				return new SetGeometryResult(guid, nrIndices * 3, geometry.getNrVertices(), binaryIndexBuffer, binaryVertexBuffer);
+			}
+		} finally {
+			model.close();
+		}
+		return null;
+	}
+
+
+}
